87ee62d8c047beb06c34abf3b0f5e8c93e217714
5826dodo <rjdonado29@gmail.com>
2026-02-05 17:35:49 -0400

    modificando ancho de la imagen logo

--- app/cotizar/page.tsx
+++ app/cotizar/page.tsx
@@ -91,7 +91,7 @@ export default function CotizarPage() {
       const logoUrl = '/logo3_ferremateriales.png';
       try {
         // Reducimos un poco el ancho a 45 para que no pise el texto de la derecha
-        doc.addImage(logoUrl, 'PNG', 10, 10, 50, 60);
+        doc.addImage(logoUrl, 'PNG', 10, 10, 45, 45);
       } catch (e) {
         console.error('Error logo', e);
       }
