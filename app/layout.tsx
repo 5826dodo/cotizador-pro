7aa79d436463c23626b471237c420c797d8cde8c
5826dodo <rjdonado29@gmail.com>
2026-02-19 09:34:10 -0400

    mejorando la vista del login

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: 'Cotizador Pro',
+  title: 'VentiQ',
   description: 'Sistema de gestión de inventario y clientes',
 };
 
