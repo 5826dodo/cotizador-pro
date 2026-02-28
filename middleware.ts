d4d5637f6e052e5d9b6909b727532254bb032327
5826dodo <rjdonado29@gmail.com>
2026-02-28 11:08:19 -0400

    agregando boton de compartir link de catalogo

--- middleware.ts
+++ middleware.ts
@@ -89,5 +89,6 @@ export const config = {
    */
   matcher: [
     '/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    '/((?!api|_next/static|_next/image|favicon.ico|catalogo).*)',
   ],
 };
